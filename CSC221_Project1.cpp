1f34caf43cd4b5cd5dfb1fd343440da8ed93853d
aslihancelik <aslihancelik27@gmail.com>
2024-10-10 08:19:52 -0400

    initiated

--- CSC221_Project1.cpp
+++ CSC221_Project1.cpp
@@ -1,20 +1,27 @@
 // CSC221_Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//
+//
+//
+//Programmer: Aslihan Celik
+//Date: 10/10/2024
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int monthlySales, advancedPay;
+
+	cout << "Enter the salesperson's monthly sales: " << endl;
+	cin >> monthlySales;
+	cout << "Enter the amount of advanced pay for this salesperson: " << endl;
+	cin >> advancedPay;
+
+	//If monthly sales negative enter again
+	//If advanced pay is negative or more than $1,500 enter again
+	
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
